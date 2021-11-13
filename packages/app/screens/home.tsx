459569dcef88604926b385b8ac95d525c4eec432
Jun Kyu <kuk941025@gmail.com>
2021-11-13 17:17:30 +0900

    Add versus icon

--- packages/app/screens/home.tsx
+++ packages/app/screens/home.tsx
@@ -1,24 +1,30 @@
 import React from 'react'
-import { Text, View, Image } from 'react-native'
+import { View, Image } from 'react-native'
 import tw from 'twrnc'
+import VersusIcon from 'app/components/VersusIcon'
 
 export default function HomeScreen() {
   return (
     <View style={tw`flex flex-1 p-4`}>
-      <Text style={tw`text-white`}>Hi</Text>
-      <View style={tw`flex flex-row h-60`}>
+      <View style={tw.style(`flex flex-row`, { height: '80%' })}>
         <Image
           source={{
             uri: 'https://image.shutterstock.com/image-illustration/temporary-permanent-choice-life-pictured-600w-1726904257.jpg'
           }}
-          style={tw`flex-1 h-20`}
+          style={tw`flex-1 h-full`}
         />
         <Image
           source={{
             uri: 'https://image.shutterstock.com/image-illustration/temporary-permanent-choice-life-pictured-600w-1726904257.jpg'
           }}
-          style={tw`flex-1 h-20`}
+          style={tw`flex-1 h-full`}
         />
+
+        <View
+          style={tw`absolute left-0 right-0 top-0 bottom-0 justify-center items-center`}
+        >
+          <VersusIcon />
+        </View>
       </View>
     </View>
   )
