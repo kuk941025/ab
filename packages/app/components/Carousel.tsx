254a7183c1bfb749dd8b2382e84a9d4a93c3b4aa
Jun Kyu <kuk941025@gmail.com>
2021-11-13 20:36:07 +0900

    Extract Carousel component

---
+++ packages/app/components/Carousel.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Dimensions } from 'react-native'
+import NativeCarousel, {
+  CarouselProps as NativeCarouselProps
+} from 'react-native-snap-carousel'
+
+interface CarouselProps {
+  renderItem: () => JSX.Element
+}
+const Carousel: React.FC<CarouselProps> = ({ renderItem }) => {
+  const { height, width } = Dimensions.get('window')
+  return (
+    <NativeCarousel
+      data={[1, 2]}
+      renderItem={renderItem}
+      sliderWidth={width}
+      itemWidth={width}
+      layout="default"
+      vertical
+      itemHeight={height}
+      sliderHeight={height}
+    />
+  )
+}
+
+export default Carousel
--- packages/app/screens/home.tsx
+++ packages/app/screens/home.tsx
@@ -1,25 +1,16 @@
 import React from 'react'
-import { View, Dimensions } from 'react-native'
-import Carousel from 'react-native-snap-carousel'
+import { View } from 'react-native'
+
 import tw from 'twrnc'
+import Carousel from 'app/components/Carousel'
 import Article from 'app/components/Article'
 
 export default function HomeScreen() {
-  const {height, width} = Dimensions.get('window')
   const renderArticle = () => <Article />
 
   return (
     <View style={tw`flex-1`}>
-      <Carousel
-        data={[1, 2]}
-        renderItem={renderArticle}
-        sliderWidth={width}
-        itemWidth={width}
-        layout="default"
-        vertical
-        itemHeight={height}
-        sliderHeight={height}
-      />
+      <Carousel renderItem={renderArticle} />
     </View>
   )
 }
