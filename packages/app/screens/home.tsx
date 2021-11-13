db5cdcc224a112c0ae17836412124060c1aa63b0
Jun Kyu <kuk941025@gmail.com>
2021-11-13 16:06:08 +0900

    Set images

--- packages/app/screens/home.tsx
+++ packages/app/screens/home.tsx
@@ -1,7 +1,25 @@
 import React from 'react'
-import { Text } from 'react-native'
+import { Text, View, Image } from 'react-native'
 import tw from 'twrnc'
 
 export default function HomeScreen() {
-  return <Text style={tw`bg-blue-200`}>Asd</Text>
+  return (
+    <View style={tw`flex flex-1 p-4`}>
+      <Text style={tw`text-white`}>Hi</Text>
+      <View style={tw`flex flex-row h-60`}>
+        <Image
+          source={{
+            uri: 'https://image.shutterstock.com/image-illustration/temporary-permanent-choice-life-pictured-600w-1726904257.jpg'
+          }}
+          style={tw`flex-1 h-20`}
+        />
+        <Image
+          source={{
+            uri: 'https://image.shutterstock.com/image-illustration/temporary-permanent-choice-life-pictured-600w-1726904257.jpg'
+          }}
+          style={tw`flex-1 h-20`}
+        />
+      </View>
+    </View>
+  )
 }
